8a99e48df0324d676cc3a8d0b9aac39ee9aa6353
Alex Irvine <Alex.B.Irvine@gmail.com>
2022-04-01 09:11:17 -0400

    Updated site author to remove real name

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,7 +2,7 @@ export const siteTitle = '6 Pak AL'
 export const siteDescription = '6 Pak\'s personal site.'
 export const siteURL = '6PakAL.com'
 export const siteLink = 'https://6PakAL.com'
-export const siteAuthor = 'Alex Irvine'
+export const siteAuthor = '6 Pak AL'
 
 // Controls how many posts are shown per page on the main blog index pages
 export const postsPerPage = 10
